62195e7914ebcb3325b6583038840e3e7eb07e94
Marcelo Augusto de Almeida <marcelo777_augusto@hotmail.com>
2020-05-01 14:38:13 -0300

    adding container with dynamodb

--- deploy.py
+++ deploy.py
@@ -1,6 +1,11 @@
 import os
+import argparse
 
 APP_VERSION = '1.0.0'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--type', required=False, help='all, ddb', default='all')
+args = parser.parse_args()
 
 lines = []
 with open('docker-compose-base.yaml') as file:
@@ -10,5 +15,10 @@ with open('docker-compose-base.yaml') as file:
     with open('docker-compose.yaml', 'w') as f:
         f.write(template.format(version=APP_VERSION))
 
-os.system('docker build -t game-store:{} .'.format(APP_VERSION))
-os.system('docker-compose up -d')
+if args.type in ['all', 'ddb']:
+    if args.type == 'all':
+        os.system('docker build -t game-store:{} .'.format(APP_VERSION))
+        os.system('docker-compose up -d')
+    else:
+        os.system('docker-compose up -d dynamo')
+

1db82ab61a5a117ac38b9c84ba81970ce3c7babd
Marcelo Augusto de Almeida <marcelo777_augusto@hotmail.com>
2020-08-27 22:25:05 -0300

    adding fail test

--- test/test_platform_service.py
+++ test/test_platform_service.py
@@ -1,5 +1,6 @@
 import pytest
 from service.platform_service import create_platform
+from werkzeug.exceptions import HTTPException
 
 
 @pytest.mark.parametrize('body', [
@@ -15,3 +16,14 @@ def test_create_platform_with_success(body, mocker):
     assert response
     assert 'error' not in response.keys()
     assert 'platformId' in response.keys()
+
+
+@pytest.mark.parametrize('body', [
+    ({'name': 'xbox360', 'platformId': '123-123'})
+])
+def test_create_platform_with_fail(body, mocker):
+    mocker.patch('boto3.resource')
+    mocker.patch('service.platform.PlatformRepository.search', return_value=[])
+    mocker.patch('service.platform.PlatformRepository.get', return_value=None)
+    with pytest.raises(HTTPException):
+        create_platform(request=body)

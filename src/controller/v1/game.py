61380903f23f9bb05204adadbd1a6075501bd917
Marcelo Augusto de Almeida <marcelo777_augusto@hotmail.com>
2020-11-15 13:46:43 -0300

    adding account and GET method.

--- src/controller/v1/game.py
+++ src/controller/v1/game.py
@@ -3,7 +3,7 @@ import logging
 from flask import make_response
 
 from configuration.decorators import handler_exception
-from service.game_service import create_game, update_game
+from service.game_service import create_game, update_game, get_game_by_id
 
 
 @handler_exception
@@ -18,19 +18,20 @@ def post(body: dict):
 
 
 @handler_exception
-def get(game_id: str):
-    return 'get game Connexion! {}'.format(game_id)
+def get(account: str, game_id: str):
+    return __create_response(response=get_game_by_id(account=account, game_id=game_id))
 
 
 @handler_exception
-def put(game_id: str, body: dict):
+def put(account: str, game_id: str, body: dict):
     logging.info(f'Updating game. GameId:{game_id}')
     body['gameId'] = game_id
+    body['account'] = account
     return __create_response(response=update_game(request=body), success_status=200)
 
 
 @handler_exception
-def delete(game_id: str):
+def delete(account: str, game_id: str):
     return 'delete game Connexion! {}'.format(game_id)
 
 
